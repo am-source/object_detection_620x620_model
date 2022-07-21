6c60ab903e93691a0063881cf0781104dc916145
am-source <a.mardini@gmx.net>
2022-07-21 08:51:58 +0200

    tmp insert

--- hoch_regal_lager.py
+++ hoch_regal_lager.py
@@ -33,6 +33,15 @@ class Hochregallager:
         self.behaelter_obj_list = []
 
     def handle_grid_coordinates_and_pos_assignment(self):
+
+        # ARUCO
+        # aruco_params = cv2.aruco.DetectorParameters_create()
+        # aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+        #####
+        # aruco_coord, _, _ = cv2.aruco.detectMarkers(image_np_with_detections, aruco_dict, parameters=aruco_params)
+        # cv2.polylines(image_np_with_detections, np.int64(aruco_coord), True, (0, 255, 100), 4)
+        #####
+
         self.initialize_grid_coordinates()
         coord.handle_grid_positions(self)
 
