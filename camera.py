76497244b7c7da2bbedccb4dd5d8f3e8345c4fe2
am-source <a.mardini@gmx.net>
2022-08-02 15:43:44 +0200

    added resolution option to camera

--- camera.py
+++ camera.py
@@ -2,9 +2,14 @@ import cv2
 
 
 class MyCamera:
-    def __init__(self, cam_source=0):
+    def __init__(self, cam_source=0, res_width=1920, res_height=1080):
         self.camera = cv2.VideoCapture(cam_source)
         self.frame = None
+        self.set_res(res_width, res_height)  # For 1080P
+
+    def set_res(self, width, height):
+        self.camera.set(3, width)
+        self.camera.set(4, height)
 
     def frame_available(self):
         ret, frame = self.camera.read()
